5e57effbc3309332a7a95d9f191d62848a2f52f2
MontolioV <montoliov@gmail.com>
2018-11-20 18:06:21 +0200

    HW 08 html, css, tasks 1 and 2.

---
+++ 08/js/tasks.js
@@ -0,0 +1,125 @@
+;'use strict';
+
+function task1() {
+    let a = parseFloat(prompt('Enter first number'));
+    let b = parseFloat(prompt('Enter second number'));
+    if (isNaN(a) || isNaN(b)) {
+        alert('Enter valid numbers, please!');
+    } else if (a === b) {
+        alert('Numbers are equal');
+    } else {
+        alert(a > b ? a : b + ' is bigger');
+    }
+}
+
+function task2() {
+    task1();
+}
+
+function task3() {
+
+}
+
+function task4() {
+
+}
+
+function task5() {
+
+}
+
+function task6() {
+
+}
+
+function task7() {
+
+}
+
+function task8() {
+
+}
+
+function task9() {
+
+}
+
+function task10() {
+
+}
+
+function task11() {
+
+}
+
+function task12() {
+
+}
+
+function task13() {
+
+}
+
+function task14() {
+
+}
+
+function task15() {
+
+}
+
+function task16() {
+
+}
+
+function task17() {
+
+}
+
+function task18() {
+
+}
+
+function task19() {
+
+}
+
+function task20() {
+
+}
+
+function task21() {
+
+}
+
+function task22() {
+
+}
+
+function task23() {
+
+}
+
+function task24() {
+
+}
+
+function task25() {
+
+}
+
+function task26() {
+
+}
+
+function task27() {
+
+}
+
+function task28() {
+
+}
+
+function task29() {
+
+}
